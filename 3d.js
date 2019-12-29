d88e001706704675c21f031e1c26dd3c3a16d1df
Björn Henriksson <hello@bjornhenriksson.se>
2019-12-29 16:42:07 +0100

    only render picking buffer on click

--- 3d.js
+++ 3d.js
@@ -295,58 +295,58 @@ function drawScene(
 
   let pixels;
 
-  // render picking colors
-  buffersCollection.forEach(buffers => {
-    //start of cube
-    {
-      const numComponents = 3;
-      const type = gl.FLOAT;
-      const normalize = false;
-      const stride = 0;
-      const offset = 0;
-
-      gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
-      gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexPosition,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset
-      );
-      gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
-    }
-
-    {
-      const numComponents = 4;
-      const type = gl.FLOAT;
-      const normalize = false;
-      const stride = 0;
-      const offset = 0;
-
-      gl.bindBuffer(gl.ARRAY_BUFFER, buffers.picking);
-      gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexColor,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset
-      );
-      gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
-    }
-
-    {
-      const vertexCount = 36;
-      const type = gl.UNSIGNED_SHORT;
-      const offset = 0;
-      gl.drawArrays(gl.TRIANGLES, offset, vertexCount);
-    }
-
-    //end of cube
-  });
-
   if (clickPosition) {
+    // render picking colors
+    buffersCollection.forEach(buffers => {
+      //start of cube
+      {
+        const numComponents = 3;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+        gl.vertexAttribPointer(
+          programInfo.attribLocations.vertexPosition,
+          numComponents,
+          type,
+          normalize,
+          stride,
+          offset
+        );
+        gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+      }
+
+      {
+        const numComponents = 4;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffers.picking);
+        gl.vertexAttribPointer(
+          programInfo.attribLocations.vertexColor,
+          numComponents,
+          type,
+          normalize,
+          stride,
+          offset
+        );
+        gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
+      }
+
+      {
+        const vertexCount = 36;
+        const type = gl.UNSIGNED_SHORT;
+        const offset = 0;
+        gl.drawArrays(gl.TRIANGLES, offset, vertexCount);
+      }
+
+      //end of cube
+    });
+
     pixels = new Uint8Array(4);
 
     gl.readPixels(
